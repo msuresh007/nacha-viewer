7e6aee438318fff9845b7fb5f8f80d6484575440
Suresh Madadha <msuresh007@gmail.com>
2021-02-08 06:09:32 +0530

    Update runTest.ts
    updating test files

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -4,7 +4,7 @@ import { runTests } from 'vscode-test';
 
 async function main() {
 	try {
-		// The folder containing the Extension Manifest package.json
+		// The folder containing the Extension Manifest package.json 
 		// Passed to `--extensionDevelopmentPath`
 		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
 
